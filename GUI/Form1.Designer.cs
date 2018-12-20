ee27580727d1f1338e91d2ea087d47fa735cb665
Kotstar1 <henry.walker.01@outlook.com>
2018-12-20 14:24:29 +0100

    Änderungen

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -377,7 +377,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1427, 790);
+            this.ClientSize = new System.Drawing.Size(711, 580);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panelnav);
